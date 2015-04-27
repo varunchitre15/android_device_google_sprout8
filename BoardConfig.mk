0348facf2e2061e85e3ec238ccbba68a07d7f555
Varun Chitre <varun.chitre15@gmail.com>
2015-04-27 02:48:24 -0400

    sprout: initial bringup

---
+++ BoardConfig.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+LOCAL_PATH := device/google/sprout8
+
+# inherit from common sprout repo
+-include device/google/sprout-common/BoardConfigCommon.mk
+
+TARGET_OTA_ASSERT_DEVICE := sprout,sprout8
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/root/init.sprout.rc:root/init.sprout.rc \
+    $(LOCAL_PATH)/rootdir/root/fstab.sprout:root/fstab.sprout
+
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/root/fstab.sprout
